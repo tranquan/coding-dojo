abe4b65985e1fa0dd0e9cf2266e894960237a352
Tran Quan <tranquan@users.noreply.github.com>
2022-04-24 16:12:43 +0200

    async task with iterator

--- js/iterator.js
+++ js/iterator.js
@@ -2,8 +2,10 @@
  * Learning about Iterator and Generator
  */
 
+// ---------------------------------------------
+// simple iterator
 function* makeIterator() {
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 5; i++) {
     yield i;
   }
 }
@@ -16,15 +18,8 @@ function test1() {
   console.log(iter.next());
 }
 
-function* fibonacciGenerator() {
-  let current = 0;
-  let next = 1;
-  while (true) {
-    yield current;
-    [current, next] = [next, current + next];
-  }
-}
-
+// ---------------------------------------------
+// Custom iterator
 class FibonacciGenerator {
   current = 0;
   next = 1;
@@ -57,6 +52,63 @@ function test2() {
 }
 
 // ---------------------------------------------
+// async with iterator
+
+// the purpose is making timeout work sync
+function timeout(callback) {
+  setTimeout(() => {
+    callback(Math.floor(Math.random() * 100));
+  }, 1000);
+}
+
+function timeoutWrapper() {
+  return function (callback) {
+    timeout(callback);
+  };
+}
+
+// idea is using yield to pause execution in runTask so we can return the value in next() call
+function runTask(taskDef) {
+  let task = taskDef();
+  let result = task.next();
+
+  function step() {
+    if (!result.done) {
+      if (typeof result.value === "function") {
+        result.value(function (data) {
+          result = task.next(data);
+          step();
+        });
+      } else {
+        result = task.next();
+        step();
+      }
+    }
+  }
+
+  step();
+}
+
+function test3() {
+  console.log("---test3 start");
+
+  // old way
+  // timeout((result) => {
+  //   console.log("---timeout done! result: ", result);
+  // });
+
+  // new way, no need callback
+  runTask(function* () {
+    console.log("---runTask start");
+    const result = yield timeoutWrapper();
+    console.log("---runTask done, result: ", result);
+  });
+
+  console.log("---test3 end");
+}
+
+// ---------------------------------------------
 // Main
 // test1();
-test2();
+// test2();
+test3();
